f11fa06b93fce6ff9d2f3cce66b6b826e57fe740
klandwehr <36046960+klandwehr@users.noreply.github.com>
2018-06-21 16:28:48 -0400

    test 5

--- js/conversion.js
+++ js/conversion.js
@@ -1,2 +1,17 @@
-let d = new Date();
-document.body.innerHTML = "<h1>Today's date is: " + d + "</h1>"
+function base10_to_base64(num) {
+    var order = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_-";
+    var base = order.length;
+    var str = "", r;
+    while (num) {
+        r = num % base
+        num -= r;
+        num /= base;
+        str = order.charAt(r) + str;
+    }
+    return str;
+}
+
+
+let timeStampInMs  = window.performance && window.performance.now && window.performance.timing && window.performance.timing.navigationStart ? window.performance.now() + window.performance.timing.navigationStart : Date.now();
+
+document.body.innerHTML = "<h1>Job #: " + base10_to_base64(timeStampInMs) + "</h1>"
